d6e37598496128987adec1fa7334ec970ce20c21
Yanick Ouellet <yanickouellet555@gmail.com>
2015-04-12 12:34:09 -0400

    Add run offer api

--- carpooling_api.py
+++ carpooling_api.py
@@ -7,6 +7,7 @@ from protorpc import remote
 from google.appengine.ext import ndb
 
 from models import RunRequest
+from models import RunOffer
 
 from geopy import geocoders
 
@@ -22,15 +23,18 @@ class CarpoolingApi (remote.Service):
         geocoder = geocoders.Nominatim()
 
         location = geocoder.geocode(run_request.fromAddress)
-        run_request.fromCoord = ndb.GeoPt(location.latitude, location.longitude)
 
         if location is None:
-            raise endpoints.BadRequestException("L'adresse de destination est invalide")
+            raise endpoints.BadRequestException(
+                "L'adresse de depart est invalide")
+
+        run_request.fromCoord = ndb.GeoPt(location.latitude, location.longitude)
 
         location = geocoder.geocode(run_request.toAddress)
 
         if location is None:
-            raise endpoints.BadRequestException("L'adresse de depart est invalide")
+            raise endpoints.BadRequestException(
+                "L'adresse de destination est invalide")
 
         run_request.toCoord = ndb.GeoPt(location.latitude, location.longitude)
 
@@ -39,6 +43,34 @@ class CarpoolingApi (remote.Service):
 
     @RunRequest.query_method(path='requests', name='runrequest.list')
     def RunRequestList(self, query):
+        return query
+
+    @RunOffer.method(path='offers', http_method='POST',
+                     name='runoffer.insert')
+    def RunOfferInsert(self, run_offer):
+        geocoder = geocoders.Nominatim()
+
+        location = geocoder.geocode(run_offer.fromAddress)
+
+        if location is None:
+            raise endpoints.BadRequestException(
+                "L'adresse de depart est invalide")
+
+        run_offer.fromCoord = ndb.GeoPt(location.latitude, location.longitude)
+
+        location = geocoder.geocode(run_offer.toAddress)
+
+        if location is None:
+            raise endpoints.BadRequestException(
+                "L'adresse de destination est invalide")
+
+        run_offer.toCoord = ndb.GeoPt(location.latitude, location.longitude)
+
+        run_offer.put()
+        return run_offer
+
+    @RunOffer.query_method(path='offer', name='runoffer.list')
+    def RunOfferList(self, query):
         return query
 
 APPLICATION = endpoints.api_server([CarpoolingApi], restricted=False)
--- models.py
+++ models.py
@@ -14,3 +14,18 @@ class RunRequest(EndpointsModel):
 
     fromCoord = ndb.GeoPtProperty()
     toCoord = ndb.GeoPtProperty()
+
+
+class RunOffer(EndpointsModel):
+    fromAddress = ndb.StringProperty()
+    toAddress = ndb.StringProperty()
+    hour = ndb.IntegerProperty()
+    minute = ndb.IntegerProperty()
+    dayOfWeek = ndb.IntegerProperty()
+    date = ndb.DateTimeProperty()
+    ponctual = ndb.BooleanProperty()
+    places = ndb.IntegerProperty()
+    kmValue = ndb.IntegerProperty()
+
+    fromCoord = ndb.GeoPtProperty()
+    toCoord = ndb.GeoPtProperty()
